acd8f3ec6c6936d3855d0c5663c704b37deb8c95
milikkarty <milikkarty@gmail.com>
2022-07-18 16:49:33 +0300

    Map through contacts

--- src/Footer.js
+++ src/Footer.js
@@ -6,33 +6,51 @@ const handleMailClick = () => {
 };
 
 const Footer = () => {
-  return (
-    <section>
-        <h1>Contact me</h1>
-        <div className="wrapper">
-            <a href="https://t.me/capturedamericanspy" className="icon telegram" target="_blank" rel="noreferrer">
-                <span className="tooltip">Telegram</span>
-                <span><FontAwesomeIcon icon={faTelegram} /></span>
-            </a>
-            <a href="https://www.linkedin.com/in/klymenko/" className="icon linkedin" target="_blank" rel="noreferrer">
-                <span className="tooltip">LinkedIn</span>
-                <span><FontAwesomeIcon icon={faLinkedin} /></span>
-            </a>
-            <a href="https://github.com/milikkarty" className="icon github" target="_blank" rel="noreferrer">
-                <span className="tooltip">Github</span>
-                <span><FontAwesomeIcon icon={faGithub} /></span>
-            </a>
-            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" className="icon youtube" target="_blank" rel="noreferrer">
-                <span className="tooltip">Youtube</span>
-                <span><FontAwesomeIcon icon={faYoutube} /></span>
-            </a>
-            <div className="icon email" id="copyEmail" onClick={handleMailClick}>
-                <span className="tooltip">Email</span>
-                <span><FontAwesomeIcon icon={faGoogle} /></span>
+
+    const contacts = [
+        {
+            href: "https://t.me/capturedamericanspy",
+            className: "icon telegram",
+            name: "Telegram",
+            icon: faTelegram
+        },
+        {
+            href: "https://www.linkedin.com/in/klymenko/",
+            className: "icon linkedin",
+            name: "LinkedIn",
+            icon: faLinkedin
+        },
+        {
+            href: "https://github.com/milikkarty",
+            className: "icon github",
+            name: "Github",
+            icon: faGithub
+        },
+        {
+            href: "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley",
+            className: "icon youtube",
+            name: "Youtube",
+            icon: faYoutube
+        }
+    ]
+
+    return (
+        <section>
+            <h1>Contact me</h1>
+            <div className="wrapper">
+                { contacts.map(contact => (
+                    <a href={contact.href} className={contact.className} target="_blank" rel="noreferrer">
+                        <span className="tooltip">{contact.name}</span>
+                        <span><FontAwesomeIcon icon={contact.icon} /></span>
+                    </a>
+                ))}
+                <div className="icon email" id="copyEmail" onClick={handleMailClick}>
+                    <span className="tooltip">Email</span>
+                    <span><FontAwesomeIcon icon={faGoogle} /></span>
+                </div>
             </div>
-        </div>
-    </section>
-  )
+        </section>
+    )
 }
 
 export default Footer;
